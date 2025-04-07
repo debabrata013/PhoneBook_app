8728636a1edef88a4494f2fe6c6194c835ca8e32
DEV <pattnaikd833@gmail.com>
2025-04-07 19:47:25 +0530

    feat: Integrate Firebase Realtime Database and update build configurations
    This commit introduces Firebase Realtime Database to the project and updates the build configurations accordingly. The key changes include:
    
    -   Added `firebase-database` library dependency in `gradle/libs.versions.toml`.
    -   Added `google-gms-google-services` plugin to the root `build.gradle.kts` and `app/build.gradle.kts`.
    -   Updated `app/build.gradle.kts` to include the Firebase Database dependency and enable view binding.
    -   Updated `MainActivity.kt` to initialize a `DatabaseReference` and interact with Firebase Realtime Database.
    - Added `viewBinding = true` in `buildFeatures` of app `build.gradle.kts`.
    - Added click listener on `tvDisplay` in `MainActivity.kt` to set value for firebaseRef.

--- app/src/main/java/com/example/phonebook/MainActivity.kt
+++ app/src/main/java/com/example/phonebook/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.phonebook
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.phonebook.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseRef = FirebaseDatabase.getInstance().getReference("users") // "users" is the node
+
+        binding.tvDisplay.setOnClickListener {
+            firebaseRef.setValue("hello")
+            firebaseRef.setValue("world")
         }
     }
 }
